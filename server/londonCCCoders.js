16c480d2b8a24358b7cedbe126de609098e4e97b
danii-nebot <daniign@gmail.com>
2013-11-24 00:26:08 +0000

    Change profile API call to be sync, return data

--- server/londonCCCoders.js
+++ server/londonCCCoders.js
@@ -1,28 +1,27 @@
 if (Meteor.isServer) {
-  RESTstop.configure();  
+  RESTstop.configure();
 
   // add restful API method
   // to be invoked from Google Docs :)
   RESTstop.add('get_user_list', function() {
-    var membersAPIurl = 
+    var membersAPIurl =
       'http://api.meetup.com/2/members' +
-      '?order=name' + 
-      '&group_urlname=London-Climate-Change-Coders' + 
+      '?order=name' +
+      '&group_urlname=London-Climate-Change-Coders' +
       '&offset=0' +
       '&page=200' +
       '&sign=true'+
       '&key='+APIKEY;
-      
-    
+
     var membersResponse = HTTP.get(membersAPIurl);
-    
+
     var resultArray = [];
     // extracts member ids from response and fetch group profile info
     // for each member
     var numMembers = membersResponse.data.results.length;
     for (var i=0; i<numMembers; i++) {
       var id = membersResponse.data.results[i].id;
-      var profileAPIurl = 
+      var profileAPIurl =
         'http://api.meetup.com/2/profiles'+
         '?order=visited'+
         '&group_urlname=London-Climate-Change-Coders'+
@@ -30,29 +29,33 @@ if (Meteor.isServer) {
         '&offset=0'+
         '&sign=true'+
         '&key='+APIKEY;
-      
-      HTTP.get(profileAPIurl, function(error, response) {
-        var responseData = response.data.results[0];
-        // pluck answer to tech skills question
-        var techQuestion = _.find(responseData.answers, function(obj) {
-          return obj.question.indexOf("techy" !== -1);
-        });
-        // could be null if not answered
-        var actualAnswer = (techQuestion ? techQuestion.answer : '');
 
-        // push to result array
-        resultArray.push({
-          name : responseData.name, 
-          bio : responseData.bio,
-          answer: actualAnswer
-        });
+      var response = HTTP.get(profileAPIurl)
+
+      var responseData = response.data.results[0];
+      // pluck answer to tech skills question
+      var techQuestion = _.find(responseData.answers, function(obj) {
+        return obj.question.indexOf("techy" !== -1);
       });
+      // could be null if not answered
+      var actualAnswer = (techQuestion ? techQuestion.answer : '');
+
+      // create JSON object
+      var jsonObj = {
+        name : responseData.name,
+        bio : responseData.bio,
+        answer: actualAnswer
+      };
+
+      // push to result array
+      resultArray.push(jsonObj);
+
+      // if finished, return data
+      if(resultArray.length === numMembers) {
+        return { results: resultArray};
+      }
     }
 
-    // return
-    return {
-      results: resultArray
-    };
   });
 
 }
