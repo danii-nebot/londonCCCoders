da96bade890a708c5d2a4569f15ad4800eea01cb
danii-nebot <daniign@gmail.com>
2013-11-30 00:16:58 +0000

    Big refactor to go full async to avoid being throttled by meet up API
    Using Fiber Futures: https://github.com/laverdet/node-fibers#futures

--- server/londonCCCoders.js
+++ server/londonCCCoders.js
@@ -4,23 +4,58 @@ if (Meteor.isServer) {
   // add restful API method
   // to be invoked from Google Docs :)
   RESTstop.add('get_members_list', function() {
-    var membersAPIurl =
-      'http://api.meetup.com/2/members' +
-      '?order=name' +
-      '&group_urlname=London-Climate-Change-Coders' +
-      '&offset=0' +
-      '&page=200' +
-      '&sign=true'+
-      '&key='+APIKEY;
+    // response object
+    var response = this.response;
+    response.writeHead(200, {'Content-Type':'text/json'});
 
-    var membersResponse = HTTP.get(membersAPIurl);
+    // Fiber Future to wait on async calls :)
+    var Future = Npm.require('fibers/future'), wait = Future.wait, Fiber = Npm.require('fibers');
+    var getMembersF = Future.wrap(getMembers);
+    var getMemberProfileF = Future.wrap(getMemberProfile);
 
-    var resultArray = [];
-    // extracts member ids from response and fetch group profile info
-    // for each member
-    var numMembers = membersResponse.data.results.length;
-    for (var i=0; i<numMembers; i++) {
-      var id = membersResponse.data.results[i].id;
+    var fut = new Future();
+    Fiber(function() {
+      // Return the results
+      var membersData = getMembersF();
+      wait(membersData);
+
+      var resultsArray = [];
+      // TODO: get this working!
+      // for now in order to avoid getting throttled again
+      var numMembers = 2; // membersData.value.length;
+      for (var ii = 0; ii < numMembers; ++ii) {
+        var member = membersData.value[ii];
+        resultsArray.push(getMemberProfileF(member.id));
+      }
+      wait(resultsArray);
+
+      response.end(JSON.stringify({results: resultsArray}));
+      fut.return();
+
+    }).run();
+
+    fut.wait();
+
+  });
+
+  function getMembers (callback) {
+      var membersAPIurl =
+        'http://api.meetup.com/2/members' +
+        '?order=name' +
+        '&group_urlname=London-Climate-Change-Coders' +
+        '&offset=0' +
+        '&page=200' +
+        '&sign=true'+
+        '&key='+APIKEY;
+
+      HTTP.get(membersAPIurl, function(err, response) {
+        callback(err,response.data.results);
+      });
+  }
+
+  function getMemberProfile(id, callback) {
+    // set timeout to avoid getting throttled by meetup API
+    Meteor.setTimeout(function() {
       var profileAPIurl =
         'http://api.meetup.com/2/profiles'+
         '?order=visited'+
@@ -30,35 +65,31 @@ if (Meteor.isServer) {
         '&sign=true'+
         '&key='+APIKEY;
 
-      var response = HTTP.get(profileAPIurl)
+        HTTP.get(profileAPIurl, function(err,response) {
+          var responseData = response.data.results[0];
+          var techQuestion = null;
+          var ccQuestion = null;
+          // pluck answers to both questions in profile
+          _.each(responseData.answers, function(obj) {
+            if(obj.question.indexOf("techy") !== -1) {
+              techQuestion = obj;
+            } else if(obj.question.indexOf("climate") !== -1) {
+              ccQuestion = obj;
+            }
+          });
+          // create JSON object
+          var jsonObj = {
+            id: id,
+            name : responseData.name,
+            bio : responseData.bio,
+            techAnswer: (techQuestion ? techQuestion.answer : ''),
+            ccAnswer: (ccQuestion ? ccQuestion.answer : ''),
+          };
 
-      var responseData = response.data.results[0];
-
-      var techQuestion = null;
-      var ccQuestion = null;
-      // pluck answers to both questions in profile
-      _.each(responseData.answers, function(obj) {
-        if(obj.question.indexOf("techy") !== -1) {
-          techQuestion = obj;
-        } else if(obj.question.indexOf("climate") !== -1) {
-          ccQuestion = obj;
-        }
-      });
-
-      // create JSON object
-      var jsonObj = {
-        name : responseData.name,
-        bio : responseData.bio,
-        techAnswer: (techQuestion ? techQuestion.answer : ''),
-        ccAnswer: (ccQuestion ? ccQuestion.answer : ''),
-      };
-
-      // push to result array
-      resultArray.push(jsonObj);
-    }
-
-    return { results: resultArray};
-
-  });
+          // return data
+          callback(err, jsonObj);
+        });
+      }, 100);
+  }
 
 }
